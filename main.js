1b7438c3bf0e68fe9fddc8be7546b21bbb35550c
bitto005 <116060629+bitto005@users.noreply.github.com>
2022-10-18 12:46:31 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var date= new Date()
+var d = date.getDate()
+var m = date.getMonth()+1
+d=3
+m=11
+if(d==3 && m==11){
+  console.log("Birthday today")
+}else{
+  
+  document.querySelector("body").innerHTML = "<center>Today is not birthday. wait more </center>"
+}
+
+
+new TypeIt(".box", {
+  strings: ["Qzm", "Happy Birthday..","Many many happy returns of the day. May your all dreams come true. May the best day you passed be the worst in future.","Keep smiling in every situation.. If I started to praise now then you may think I'm just flirting..","So...","This was a small wish from your just friend. You may get more warm wishes from your close friends..","Your,,,","Characterless, lesbian, selfish, womanizer, guilty,playboy best friend","Sorry..","Just friend.. :)"],
+  speed: 90,
+  waitUntilVisible: true,
+}).go();
+
+
